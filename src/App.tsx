4977afd5a2c1cdfaf78a1327e144e51898fc5f69
Mahfuz <aamahfuz.pro@gmail.com>
2025-02-22 13:45:25 +0600

    added fonts and default colors and sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <h1 className="text-gray-500 font-bold text-5xl text-center">
-        This is the starting
+      <h1 className="text-gray-300 font-bold text-5xl text-center">
+        Web Developement Team
       </h1>
       <Button>Go Shadcn</Button>
     </>
