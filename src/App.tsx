00635540157ab4488715586470fadd3cc396af8f
Mahfuz <aamahfuz.pro@gmail.com>
2025-02-22 12:10:49 +0600

    COnfigured Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-blue-500 font-bold text-5xl text-center">
+      <h1 className="text-gray-500 font-bold text-5xl text-center">
         This is the starting
       </h1>
+      <Button>Go Shadcn</Button>
     </>
   );
 }
