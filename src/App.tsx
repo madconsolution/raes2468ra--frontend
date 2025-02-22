6fddaf20a7d46afb290817243c877e0a21855eb3
Mahfuz <aamahfuz.pro@gmail.com>
2025-02-22 13:54:28 +0600

    basic setup done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1 className="text-gray-300 font-bold text-5xl text-center">
-        Web Developement Team
-      </h1>
-      <Button>Go Shadcn</Button>
-    </>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-gray-800 font-semibold text-4xl sm:text-5xl md:text-6xl leading-tight mb-6">
+          Welcome Back! Let's do something exciting together!
+          <span role="img" aria-label="smiling face">
+            😊
+          </span>
+        </h1>
+        <Button className="bg-purple-600 text-white hover:bg-purple-700 px-6 py-3 rounded-lg transition-colors duration-300">
+          Start From
+        </Button>
+      </div>
+    </div>
   );
 }
 
