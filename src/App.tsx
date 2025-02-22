fda6572b11c4d768fdac5f631bb8355c5583cd95
Mahfuz <aamahfuz.pro@gmail.com>
2025-02-22 11:44:40 +0600

    Configured tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500 font-bold text-5xl text-center">
+        This is the starting
+      </h1>
+    </>
+  );
+}
+
+export default App;
